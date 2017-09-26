159779caa4a5f477cc04fbcc6a28c63b32cd871a
Zdravko Kirilov <zkirilov@kpmg.com>
2017-09-26 17:33:17 +0300

    added image fallback to article details

--- src/components/SelectedArticle/ArticleBody.js
+++ src/components/SelectedArticle/ArticleBody.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ImageFallback from 'react-image-fallback';
+const defaultImage = require('../../assets/not-available.png');
+const loader = require('../../assets/loader.gif');
+
 
 export default function ArticleBody({title, text, imageUrl, commentsCount}) {
 	return (
 		<div className="article-details">
 			<h2>{title}</h2>
-			<img className="article-details-img" src={imageUrl}/>
+			<ImageFallback
+				src={imageUrl}
+				fallbackImage={defaultImage}
+				initialImage={loader}
+				alt={title}
+				className="article-details-img"
+			/>
 			<article>{text}</article>
 		</div>
 	)
